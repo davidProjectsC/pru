ccdcebe72b4aa1cfb15f3062cff674913d940cc3
David Isrrael Chan Chan <dchan@isysa.com.mx>
2025-09-09 13:45:50 -0600

    pru

--- config/db.php
+++ config/db.php
@@ -1,24 +1,26 @@
 <?php
+
 declare(strict_types=1);
 /**
  * DB config for SQL Server (COSYSA).
  * Fill in your real server/credentials. Example DSN:
  *   sqlsrv:Server=INTELISIS;Database=COSYSA
  * Requires: php-sqlsrv + php-pdo_sqlsrv extensions installed/enabled.
-*/
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
-    $DB_DSN  = getenv('DB_DSN') ?: 'sqlsrv:Server=INTELISIS;Database=COSYSA';
-}else{
-    $DB_DSN  = getenv('DB_DSN') ?: 'dblib:host=INTELISIS;dbname=COSYSA';
-}
+ */
 $DB_USER = getenv('DB_USER') ?: 'intelisis';
 $DB_PASS = getenv('DB_PASS') ?: '';
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    $DB_DSN  = getenv('DB_DSN') ?: 'sqlsrv:Server=INTELISIS;Database=COSYSA';
+} else {
+    $DB_DSN  = getenv('DB_DSN') ?: 'dblib:host=INTELISIS;dbname=COSYSA';
+}
+
 
 try {
     $pdo = new PDO($DB_DSN, $DB_USER, $DB_PASS, [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::SQLSRV_ATTR_ENCODING    => PDO::SQLSRV_ENCODING_UTF8,
+        //PDO::SQLSRV_ATTR_ENCODING    => PDO::SQLSRV_ENCODING_UTF8,
     ]);
 } catch (Throwable $e) {
     http_response_code(500);
